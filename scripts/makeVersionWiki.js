9230493da016f6c0cf41ff8a82375bd1269e6cb2
Scott Sauyet <scott@sauyet.com>
2025-02-23 15:29:27 -0500

    Update template tiddlers with language variables, fix KJV pilcrows and inscriptions

--- scripts/makeVersionWiki.js
+++ scripts/makeVersionWiki.js
@@ -1,12 +1,13 @@
-const {mkdir, writeFile} = require('fs/promises')
+const {mkdir, writeFile, readFile} = require('fs/promises')
 const {join, resolve} = require('node:path')
 const format = require('./format')
 const nestVerses = require('./nestVerses')
 const makeTiddlers = require('./makeTiddlers')
-const makeScript = require('./makeScript')
+
+const extraTids = require('./../common/extraTids.json')
 
 module.exports = (version) => {
-  const config = (require('./loadConfig'))(version)
+  const config = require(`../versions/${version}/config`)()
   
   const enhanced = nestVerses(config)
   const tiddlers = makeTiddlers(enhanced)
@@ -28,7 +29,27 @@ module.exports = (version) => {
     .then(() => makeScript(config, tiddlers))
     .then((wikiContent) => writeFile(wikiFile, wikiContent))
     .then(() => console.log(`Wrote "${wikiFile}"`))
-    .then(() => console.log(`Completed writing "${version}"`))
+    .then(() => console.log(`\nCompleted writing "${version}"`))
     .catch(console.warn)
 }
 
+const makeScript = (config, tiddlers) => {
+  const extras = extraTids.map(update(config))
+
+  return readFile('./common/empty.html', 'utf8')
+    .then((empty) => empty.replace(
+      '<!--~~ Replace Me ~~-->', 
+      ['', ...([...extras, ...tiddlers].map(JSON.stringify))].join(',\n')
+    )
+  )
+}
+
+const update = ({title, language: {Book, Books, Chapter, Verse}}) => (tiddler) => 
+  Object.fromEntries(Object.entries(tiddler).map(([k, v]) => [
+    k, 
+    v.replaceAll('${title}', title)
+      .replaceAll('${Book}', Book)
+      .replaceAll('${Books}', Books)
+      .replaceAll('${Chapter}', Chapter)
+      .replaceAll('${Verse}', Verse)
+  ]))
--- versions/KingJames/config.js
+++ versions/KingJames/config.js
@@ -1,9 +1,18 @@
+const inscriptions = require(`../../common/inscriptions.json`)
+
+const cleanVerse = (inscriptions) => (verse) => ({
+  ...verse, 
+  text: verse.text.replace('\u00b6', '').replace(inscriptions[`${verse.book_name} ${verse.chapter}`], '').trim()
+})
+
+const verses = require('./kjv.json').verses.map(cleanVerse(inscriptions))
+
 module.exports = () => ({
   title: 'King James Bible',
   language: require(`../../common/languages/en.json`),
   metadata: require('./kjv.json').metadata,
-  verses: require('./kjv.json').verses,
+  verses,
   paragraphs: require(`../../common/paragraphs.json`),
-  inscriptions: require(`../../common/inscriptions.json`),
+  inscriptions,
   sections: require(`../../common/sections.json`)
 })
