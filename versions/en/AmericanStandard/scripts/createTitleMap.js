cf74d4238f150b34689efcce2dd72a4836f8daac
Scott Sauyet <scott@sauyet.com>
2025-02-23 00:10:10 -0500

    Complete first pass at full wiki generation

--- versions/en/AmericanStandard/scripts/createTitleMap.js
+++
@@ -1,5 +0,0 @@
-const raw = require('../output/nested.json')
-
-const books = Object.fromEntries(raw.map(b => b.title).map(b => [b, b]))
-
-console.log(JSON.stringify(books, null, 2))
--- versions/en/Coverdale/sources/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => ({
-  title: 'Coverdale Bible',
-  version: '0.1.0',
-  verses: require('./coverdale.json').verses,
-  inscriptions: require(`${process.cwd()}/common/inscriptions.json`),
-  paragraphs: require(`${process.cwd()}/common/paragraphs.json`),
-  sections: require(`${process.cwd()}/common/sections.json`),
-  bookNames: require(`${process.cwd()}/common/languages/en/bookNames.json`),
-})
