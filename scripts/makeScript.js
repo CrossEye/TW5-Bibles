3946dc7f9ee81039fbd75b100dc7dc4f93036592
Scott Sauyet <scott@sauyet.com>
2025-02-23 12:31:54 -0500

    Ensure every JSON-stringified tiddler in the wiki output is on its own line

--- scripts/makeScript.js
+++ scripts/makeScript.js
@@ -1,14 +1,19 @@
 const {readFile} = require('fs/promises')
 const extraTids = require('./../common/extraTids.json')
 
-const makeScript = (config, tiddlers) =>  
-  Promise.all([
+
+const makeScript = (config, tiddlers) => {
+  const extras = extraTids.map(update(config))
+  return Promise.all([
     readFile('./common/empty.html', 'utf8'),
     readFile('./common/extraTids.json', 'utf8')
   ]).then(
-    ([empty, extraTids]) => empty.replace('<!--~~ Replace Me ~~-->', ['', [...JSON.parse(extraTids).map(update(config)), ...tiddlers]
-      .map(JSON.stringify)].join(',\n'))
+    ([empty, extraTids]) => empty.replace(
+      '<!--~~ Replace Me ~~-->', 
+      ['', ...([...extras, ...tiddlers].map(JSON.stringify))].join(',\n')
+    )
   )
+}
 
 const update = ({title}) => (tiddler) => 
   Object.fromEntries(Object.entries(tiddler).map(([k, v]) => [k, v.replaceAll('${title}', title)]))
